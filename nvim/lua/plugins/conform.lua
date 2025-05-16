05da6d3e4b2b83aadd6e95784bc2978c3e45297c
Jonas Krogh <jonas.krogh@statnett.no>
2025-05-16 12:30:54 +0200

    Add command to disable autoformat

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,9 +1,14 @@
 require("conform").setup({
 	notify_on_error = false,
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
+	format_on_save = function(bufnr)
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		}
+	end,
 	formatters_by_ft = {
 		lua = { "stylua" },
 		javascript = { { "prettierd", "prettier" } },
@@ -11,4 +16,25 @@ require("conform").setup({
 		typescript = { { "prettierd", "prettier" } },
 		typescriptreact = { { "prettierd", "prettier" } },
 	},
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat on save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function(args)
+	if args.bang then
+		vim.b.disable_autoformat = false
+	else
+		vim.g.disable_autoformat = false
+	end
+end, {
+	desc = "Enable autoformat on save",
+	bang = true,
 })
