caa9595837c0a8c23937fd66ca9ec6e5f019755e
Jonas Krogh <jonas.krogh@statnett.no>
2025-05-16 12:29:17 +0200

    Fix some transparency in nvim

--- nvim/lua/theme/dracula.lua
+++ nvim/lua/theme/dracula.lua
@@ -4,5 +4,8 @@ vim.cmd([[
 colorscheme dracula
 hi Normal guibg=none ctermbg=none
 hi NonText guibg=none ctermbg=none
-hi NvimTreeNormal guibg=none ctermbg=none
+hi NvimTree guibg=none ctermbg=none
+hi TelescopeNormal guibg=none ctermbg=none
+hi TelescopeBorder guibg=none ctermbg=none
+hi WinSeparator guibg=none ctermbg=none
 ]])
